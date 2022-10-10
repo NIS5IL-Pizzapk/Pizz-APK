15b091657c9524ea7ef0883224779a3050dd5e1f
Qwerty <101248461+QTBoisson@users.noreply.github.com>
2022-10-10 14:43:54 +0200

    ViewBinding Implementation
    implementation des bindings dans le module gradle et binding de la mainActivity

--- app/src/main/java/com/example/pizz_apk/MainActivity.java
+++ app/src/main/java/com/example/pizz_apk/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.pizz_apk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pizz_apk.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
