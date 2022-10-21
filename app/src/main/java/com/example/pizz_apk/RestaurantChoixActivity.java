8048527db7b1768dd3a9a61a188db776ae1ba6cd
Mohamed <m.tata@intech-sud.fr>
2022-10-21 16:25:54 +0200

    Page accueil + page connexion

--- app/src/main/java/com/example/pizz_apk/RestaurantChoixActivity.java
+++ app/src/main/java/com/example/pizz_apk/RestaurantChoixActivity.java
@@ -3,33 +3,48 @@ package com.example.pizz_apk;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.pizz_apk.adapters.RestaurantsAdapter;
-import com.example.pizz_apk.databinding.ActivityMainBinding;
+import com.example.pizz_apk.databinding.ActivityRestaurantChoixBinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RestaurantChoixActivity extends AppCompatActivity {
 
+    private String[] listScrollingText;
     List<String> restaurantsList;
-    ActivityMainBinding binding;
+    ActivityRestaurantChoixBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_choix);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityRestaurantChoixBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
         restaurantsList = new ArrayList<>();
         restaurantsList.add("Clarensac");
-        restaurantsList.add("Nîmes");
+        restaurantsList.add("Vauvert");
         RestaurantsAdapter restaurantsAdapter = new RestaurantsAdapter(restaurantsList,this);
         binding.rvHomepageRestaurants.setHasFixedSize(true);
         binding.rvHomepageRestaurants.setAdapter(restaurantsAdapter);
         binding.rvHomepageRestaurants.setLayoutManager(new LinearLayoutManager(this));
+
+        TextView tv=(TextView)findViewById(R.id.scroll_text);
+        tv.setSelected(true);
+
+        //Récupération de la liste des textes à faire défiler
+        listScrollingText = getResources().getStringArray(R.array.scrolling_text);
+
+        int randomIndex = new Random().nextInt(listScrollingText.length);
+        String randomName = listScrollingText[randomIndex];
+        tv.setText(randomName);
     }
 }
