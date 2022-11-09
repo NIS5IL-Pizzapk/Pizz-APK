9861c2e1734ca0d39ec9ab31a824b1601090aaea
Minunn <minunn2000@gmail.com>
2022-11-09 10:58:46 +0100

    ajout de la navigation vers le pannier

--- app/src/main/java/com/example/pizz_apk/MainActivity.java
+++ app/src/main/java/com/example/pizz_apk/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.pizz_apk;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.ui.AppBarConfiguration;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -42,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RestaurantChoixFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_view);
         }
+
+
     }
 
     @Override
@@ -67,6 +72,16 @@ Fragment fragment = null;
                 break;
             case R.id.nav_account:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MonCompteFragment()).commit();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PannierFragment()).commit();
                 break;
         }
         return true;
