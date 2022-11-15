15fd42d89f6b8d344c8be71e4a826aa23af495e1
Minunn <minunn2000@gmail.com>
2022-11-15 16:08:24 +0100

    Started rework menu using navigation component instead of replace

--- app/src/main/java/com/example/pizz_apk/MainActivity.java
+++ app/src/main/java/com/example/pizz_apk/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -13,19 +14,26 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private DrawerLayout drawerLayout;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSupportActionBar(findViewById(R.id.toolbar));
 
+        navController = Navigation.findNavController(this, R.id.fragment_container);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //remove this if you want application to start
+        setSupportActionBar(findViewById(R.id.toolbar));
         NavigationUI.setupWithNavController((NavigationView) findViewById(R.id.navigation_view), navController);
         NavigationUI.setupActionBarWithNavController(this, navController, (DrawerLayout) findViewById(R.id.drawer_layout));
 
     }
 
+    //remove this if you want application to start
     @Override
     public boolean onSupportNavigateUp() {
         return NavigationUI.navigateUp(navController, (DrawerLayout) findViewById(R.id.drawer_layout));
