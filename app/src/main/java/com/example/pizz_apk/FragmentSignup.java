cecf1cf8265a706a2da9bea77077208d020ce4f0
Mohamed <m.tata@intech-sud.fr>
2022-11-15 11:57:27 +0100

    creation page signup

---
+++ app/src/main/java/com/example/pizz_apk/FragmentSignup.java
@@ -0,0 +1,29 @@
+package com.example.pizz_apk;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentSignup extends Fragment {
+
+    public FragmentSignup() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
