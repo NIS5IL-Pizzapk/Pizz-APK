3a2fc5daeee5752e8b1ec7fbd275820227a436ef
Minunn <minunn2000@gmail.com>
2022-11-17 09:58:06 +0100

    Finally fixed the menu and going to pages !

--- app/src/main/java/com/example/pizz_apk/MainActivity.java
+++ app/src/main/java/com/example/pizz_apk/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pizz_apk;
 
+import static androidx.navigation.Navigation.findNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -9,6 +11,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,21 +25,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        navController = Navigation.findNavController(this, R.id.fragment_container);
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        //remove this if you want application to start
         setSupportActionBar(findViewById(R.id.toolbar));
-        NavigationUI.setupWithNavController((NavigationView) findViewById(R.id.navigation_view), navController);
-        NavigationUI.setupActionBarWithNavController(this, navController, (DrawerLayout) findViewById(R.id.drawer_layout));
 
-    }
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.restaurantChoixFragment,R.id.nav_parametres, R.id.nav_apropos, R.id.nav_account, R.id.nav_contact, R.id.nav_deconnexion)
+                .setOpenableLayout(drawerLayout)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+       }
 
     //remove this if you want application to start
     @Override
     public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(navController, (DrawerLayout) findViewById(R.id.drawer_layout));
+      NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
 
