647be105449b9bdeefff476f34519cf963d8ef4a
Minunn <minunn2000@gmail.com>
2023-01-19 14:01:23 +0100

    update change LostPasswordFragment

--- app/src/main/java/com/example/pizz_apk/LostPasswordFragment.java
+++ app/src/main/java/com/example/pizz_apk/LostPasswordFragment.java
@@ -2,6 +2,8 @@ package com.example.pizz_apk;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -14,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.pizz_apk.models.RetroFitRequests;
+import com.example.pizz_apk.services.Utils;
 
 import java.util.HashMap;
 
@@ -41,23 +44,32 @@ public class LostPasswordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        this.requests = Utils.getRetrofitCon(requireContext());
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lost_password, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.sendMail(view);
     }
 
     public void sendMail(View view) {
 
         Button sendMailButton = view.findViewById(R.id.fragment_reset_btn_valid);
         EditText customerMail = view.findViewById(R.id.fragment_reset_input_mail);
-
         sendMailButton.setOnClickListener(v -> {
+
             HashMap<String, String> map = new HashMap<>();
             map.put("email", customerMail.getText().toString());
 
             requests.executeSendMail(map).enqueue(new Callback<Void>() {
                 @Override
                 public void onResponse(Call<Void> call, Response<Void> response) {
-                    if (response.isSuccessful() && customerMail != null) {
+                    Log.d("TEST", "mailer répond: " + response);
+                    if (response.isSuccessful()) {
                         Toast.makeText(getContext(), "Envoyé ! Veuillez vérifier votre boite mail", Toast.LENGTH_SHORT).show();
                         Navigation.findNavController(v).navigate(R.id.loginFragment);
                     } else if (response.code() == 400) {
